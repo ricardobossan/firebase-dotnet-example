78f64f6697115019c595fad0cf868682d423b8c7
ricardobossan <ricardobossan@protonmail.com>
2022-12-14 21:13:17 -0300

    cria mensagem de erro para quado não houver credenciais do firebase

--- Services/FirebaseService.cs
+++ Services/FirebaseService.cs
@@ -13,23 +13,30 @@ namespace Services
     {
       _config = config;
       /*
-      GetInstance(config).Child("dinosaurs").AsObservable<Dinosaur>()
-        .Subscribe(d =>
-            {
-            if (d.EventType == FirebaseEventType.InsertOrUpdate)
-            {
-            Console.WriteLine("Evento de Insert");
-            }
-            else if (d.EventType == FirebaseEventType.Delete)
-            {
-            Console.WriteLine("Evento de Delete");
-            }
-            });
-      */
+         GetInstance(config).Child("dinosaurs").AsObservable<Dinosaur>()
+         .Subscribe(d =>
+         {
+         if (d.EventType == FirebaseEventType.InsertOrUpdate)
+         {
+         Console.WriteLine("Evento de Insert");
+         }
+         else if (d.EventType == FirebaseEventType.Delete)
+         {
+         Console.WriteLine("Evento de Delete");
+         }
+         });
+         */
     }
 
     public static void MonitoraFirebase(IConfiguration config)
     {
+      //TODO: por que a requisição em essa barra entre `Dinosaur` e `.json`
+      /*TODO: Mensagem de erro:
+        Url: https://fir-874a5-default-rtdb.firebaseio.com/dinosaurs/.json?auth=[firebase_auth]
+        Request Data:
+        Response:
+        ---> System.Net.Http.HttpRequestException: Response status code does not indicate success: 401 (Unauthorized).
+      */
       GetInstance(config).Child("dinosaurs").AsObservable<Dinosaur>()
         .Subscribe(d =>
             {
@@ -64,6 +71,10 @@ namespace Services
       string auth = config["firebase_auth"];
       string baseUrl = config["firebase_url"];
 
+      if ((auth is null || baseUrl is null) || (auth == string.Empty || baseUrl == string.Empty))
+      {
+        throw new Exception("firebase authentication id or url are missing");
+      }
       FirebaseClient firebaseClient = new(
           baseUrl,
           new FirebaseOptions
